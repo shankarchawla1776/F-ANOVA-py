bf051e982df93ada0b45b394c099f89822364139
Indefeasible <139474458+shankarchawla1776@users.noreply.github.com>
2025-07-23 01:24:30 -0400

    added core.py to contain the constructor method, import only the FunctionalANOVA class from core.py in __init__.py

--- functionalANOVA/__init__.py
+++ functionalANOVA/__init__.py
@@ -1,9 +1,11 @@
-from .two_group_cov import two_group_cov
-from .k_group_cov import k_group_cov
-from .k_group_cov_pairwise import k_group_cov_pairwise
-from .one_way import one_way
-from .one_way_BF import one_way_BF
-from .two_way import two_way
-from .two_way_BF import two_way_BF
-from .function_subsetter import function_subsetter
+# from .two_group_cov import two_group_cov
+# from .k_group_cov import k_group_cov
+# from .k_group_cov_pairwise import k_group_cov_pairwise
+# from .one_way import one_way
+# from .one_way_BF import one_way_BF
+# from .two_way import two_way
+# from .two_way_BF import two_way_BF
+# from .function_subsetter import function_subsetter
 # from .timer import timer
+
+from .core import FunctionalANOVA
